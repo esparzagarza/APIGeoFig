b80c5c697e2347c490600106859eaee9c28dd807
Pedro Esparza <isc.pedroesparza@gmail.com>
2021-10-23 14:36:09 -0700

    1.0.7

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 namespace App;
 
 // Cors
-header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Origin: '*'");
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: POST, OPTIONS");
 header("Access-Control-Max-Age: 3600");
