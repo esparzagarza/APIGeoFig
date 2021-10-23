5103ad4df558a8623885771a16729cf03cfa7491
Pedro Esparza <isc.pedroesparza@gmail.com>
2021-10-23 10:41:06 -0700

    dependencies resolved to production mode

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,6 +22,8 @@ class ComposerAutoloaderInitf331b31079d51e149b8982871346f34d
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInitf331b31079d51e149b8982871346f34d', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(\dirname(__FILE__)));
         spl_autoload_unregister(array('ComposerAutoloaderInitf331b31079d51e149b8982871346f34d', 'loadClassLoader'));
